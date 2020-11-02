57d48d6549210a18c22e16d2238691e6641bdba3
Tanush2312 <70521659+Tanush2312@users.noreply.github.com>
2020-11-02 18:50:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var ball;
+var database,position
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database()
+    var ref=database.ref("ball/position")
+    ref.on("value",readop,shwerr)
+}
+function readop(data){
+position=data.val()
+ball.x=position.x
+ball.y=position.y
+}
+function shwerr(){
+    console.log("error")
+}
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+   var ref=database.ref("ball/position")
+   ref.set({
+       x:position.x+x,
+       y:position.y+y
+   })
+}
